e802844dd3461272d086dcfb264fbb8f6a027e56
Daksh <dnakra@bsd220.org>
2022-12-02 11:29:35 -0600

    Forgot to commit this apparently

--- Word Play/ContentView.swift
+++ Word Play/ContentView.swift
@@ -10,32 +10,34 @@ import SwiftUI
 struct ContentView: View {
     @State private var words = Words()
     var body: some View {
-        VStack {
-            Text("Please fill in all the text boxes with the kind of words they ask for:")
-            HStack {
-                CustomTextField(placeholder: "BodyPart", variable: $words.bodyPart)
-                CustomTextField(placeholder: "Occupation", variable: $words.occupation)
+        NavigationView {
+            VStack {
+                Text("Please fill in all the text boxes with the kind of words they ask for:")
+                HStack {
+                    CustomTextField(placeholder: "BodyPart", variable: $words.bodyPart)
+                    CustomTextField(placeholder: "Occupation", variable: $words.occupation)
+                }
+                HStack {
+                    CustomTextField(placeholder: "Object", variable: $words.object)
+                    CustomTextField(placeholder: "Procedure", variable: $words.procedure)
+                }
+                HStack {
+                    CustomTextField(placeholder: "Number", variable: $words.number)
+                    CustomTextField(placeholder: "Song Title", variable: $words.songTitle)
+                }
+                HStack {
+                    CustomTextField(placeholder: "Vehicle", variable: $words.vehicle)
+                    CustomTextField(placeholder: "Building", variable: $words.building)
+                }
+                HStack {
+                    CustomTextField(placeholder: "Second Number", variable: $words.secondNumber)
+                    CustomTextField(placeholder: "Unit Of Time", variable: $words.unitOfTime)
+                }
+                Spacer()
+                NavigationLink("Next", destination: StoryView(words: words))
             }
-            HStack {
-                CustomTextField(placeholder: "Object", variable: $words.object)
-                CustomTextField(placeholder: "Procedure", variable: $words.procedure)
-            }
-            HStack {
-                CustomTextField(placeholder: "Number", variable: $words.number)
-                CustomTextField(placeholder: "Song Title", variable: $words.songTitle)
-            }
-            HStack {
-                CustomTextField(placeholder: "Vehicle", variable: $words.vehicle)
-                CustomTextField(placeholder: "Building", variable: $words.building)
-            }
-            HStack {
-                CustomTextField(placeholder: "Second Number", variable: $words.secondNumber)
-                CustomTextField(placeholder: "Unit Of Time", variable: $words.unitOfTime)
-            }
-            Spacer()
-            NavigationLink("Next", destination: StoryView(words: words))
+            .navigationTitle("Word Play")
         }
-        .navigationTitle("Word Play")
     }
 }
 
